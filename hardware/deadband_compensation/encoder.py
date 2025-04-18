b9fad29b5273ba35e896dea90471a7710b07cf33
tanayrs <tanay@tanayrs.com>
2025-04-18 09:40:27 +0530

    deadband compensation

--- hardware/deadband_compensation/encoder.py
+++ hardware/deadband_compensation/encoder.py
@@ -53,77 +53,45 @@ class PiEncoder:
         self.pi.stop()
 
 # TrivikramEncoderProcessor remains unchanged from previous implementation
-class TrivikramEncoderProcessor:
-    def __init__(self, pulses_per_rev, play_degrees, is_wheel=True, 
-                 is_front=True, cutoff_freq=1.0, sample_time=0.001):
+class EncoderProcessor:
+    def __init__(self, pulses_per_rev,cutoff_freq=1.0):
         self.ticks_per_rev = 4 * pulses_per_rev
-        self.play_degrees = play_degrees
-        self.is_wheel = is_wheel
-        self.is_front = is_front
         self.cutoff_freq = cutoff_freq
-        self.sample_time = sample_time
         
-        if self.is_wheel:
-            self.deadband_upper = play_degrees / 2.0
-            self.deadband_lower = -play_degrees / 2.0
-        else:
-            self.deadband_upper = (play_degrees / 2.0) * self.ticks_per_rev / 360.0
-            self.deadband_lower = -self.deadband_upper
-            
-        self.adjusted_ticks = 0
         self.last_ticks = 0
+        self.position = 0
         self.speed = 0.0
-        self.bevel_ratio = 1.0
-        self.steer_ppr = 2264
 
-    def update(self, raw_ticks, steer_ticks=0, steer_offset=0):
-        if self.is_wheel:
-            self._update_wheel(raw_ticks, steer_ticks, steer_offset)
-        else:
-            self._update_steering(raw_ticks)
+        self.last_time = None
+
+    def update(self, raw_ticks):
+        self._update_wheel(raw_ticks)
             
         self._calculate_speed(raw_ticks)
 
-    def _update_wheel(self, ticks, steer_ticks, steer_offset):
-        coupled_deg = self._compute_coupled_rotation(ticks, steer_ticks, steer_offset)
-        if coupled_deg > self.deadband_upper:
-            self.adjusted_ticks += (coupled_deg - self.deadband_upper)
-            self.deadband_upper = coupled_deg
-            self.deadband_lower = coupled_deg - self.play_degrees
-        elif coupled_deg < self.deadband_lower:
-            self.adjusted_ticks += (coupled_deg - self.deadband_lower)
-            self.deadband_lower = coupled_deg
-            self.deadband_upper = coupled_deg + self.play_degrees
-
-    def _compute_coupled_rotation(self, wheel_ticks, steer_ticks, offset):
-        wheel_deg = (wheel_ticks / self.ticks_per_rev) * 360
-        steer_deg = 360 * (steer_ticks - offset) / (self.bevel_ratio * self.steer_ppr)
-        return wheel_deg - steer_deg if self.is_front else wheel_deg + steer_deg
+    def _update_wheel(self, ticks):
+        self.position = (ticks / self.ticks_per_rev) * 360
 
     def _calculate_speed(self, current_ticks):
-        delta = current_ticks - self.last_ticks
-        self.speed = (delta * 360 / self.ticks_per_rev) / self.sample_time
+        current_time = time.perf_counter()
+
+        if self.last_time is None:
+            self.last_time = current_time
+            self.last_ticks = current_ticks
+            self.speed = 0.0
+            return
+
+        delta_ticks = current_ticks - self.last_ticks
+        delta_time = current_time - self.last_time
+
+        if delta_time > 0:
+            self.speed = (delta_ticks * 360 / self.ticks_per_rev) / delta_time
+
         self.last_ticks = current_ticks
+        self.last_time = current_time
 
     def get_position_degrees(self):
-        return self.adjusted_ticks % 360
+        return self.position
 
     def get_speed(self):
         return self.speed
-    
-# Initialize hardware encoder
-encoder = PiEncoder(pin_a=23, pin_b=24)
-
-# Initialize data processor
-processor = TrivikramEncoderProcessor(
-    pulses_per_rev=1000,
-    play_degrees=5.0,
-    is_wheel=True
-)
-
-while True:
-    raw_ticks = encoder.read()
-    processor.update(raw_ticks)
-    print(f"Position: {processor.get_position_degrees():.2f}°")
-    print(f"Speed: {processor.get_speed():.2f}°/s")
-    time.sleep(0.01)
