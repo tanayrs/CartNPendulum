cdc9dd2549b432baa97096f47a0d114eab30cfb7
Tanay Raghunandan Srinivasa <tanay@tanayrs.com>
2025-04-28 19:03:21 +0530

    modified trianing env on robot

--- hardware/tuning/hardware_env.py
+++ hardware/tuning/hardware_env.py
@@ -11,12 +11,13 @@ class HardwareBalancingEnv(gym.Env):
 
     def __init__(self, max_steps=1000):
         super(HardwareBalancingEnv, self).__init__()
+        # Hardware initialization
         self.imu = MPU6050(roll_offset=0)
         self.motor = HardwarePWMMotor()
         self.encoder = PiEncoder(pin_a=23, pin_b=24)
         self.processor = EncoderProcessor(pulses_per_rev=2262)
-        
-        self.action_space = spaces.Discrete(41)
+        # Action and observation spaces
+        self.action_space = spaces.Discrete(41) # Same as simulation
         self.observation_space = spaces.Box(
             low=np.array([-2.4, -np.inf, -0.418, -np.inf]),
             high=np.array([2.4, np.inf, 0.418, np.inf]),
@@ -24,33 +25,41 @@ class HardwareBalancingEnv(gym.Env):
         )
         self.max_steps = max_steps
         self.current_step = 0
-        self.safety_threshold = 0.3
+        self.safety_threshold = 0.3 # meters
 
     def _get_obs(self):
         ticks = self.encoder.read()
         self.processor.update(ticks)
         imu_data = self.imu.update()
+        # CRITICAL FIX: imu_data['angle']['roll'] is in degrees, convert to radians ONCE
+        theta = np.deg2rad(imu_data['angle']['roll'])
+        theta_dot = np.deg2rad(imu_data['gyro']['x'])
         return np.array([
-            self.processor.get_position_meters(),
-            self.processor.get_speed_ms(),
-            np.deg2rad(imu_data['angle']['roll']),
-            np.deg2rad(imu_data['gyro']['x'])
+            self.processor.get_position_meters(),  # x
+            self.processor.get_speed_ms(),         # x_dot
+            theta,                                # theta (radians)
+            theta_dot                             # theta_dot (radians/sec)
         ], dtype=np.float32)
 
     def reset(self, seed=None, options=None):
         self.motor.stop()
         self.current_step = 0
-        input("Manually position robot upright, then press Enter...")
+        # Gentle reset procedure
+        for _ in range(50):
+            self.motor.set_speed(1000, 0)
+            time.sleep(0.01)
         return self._get_obs(), {}
 
     def step(self, action):
         self.current_step += 1
+        # Convert action to motor command
         control_output = ((action-20)/20) * 40000
         self.motor.set_speed(control_output, self.processor.get_speed_ms())
         time.sleep(0.01)
         obs = self._get_obs()
         reward = self._calculate_reward(obs, action)
-        terminated = abs(obs[0]) > self.safety_threshold or abs(obs[2]) > 0.2095
+        # Terminate if out of bounds (position or angle in radians)
+        terminated = abs(obs[0]) > self.safety_threshold or abs(obs[2]) > 0.2095 # ±12°
         truncated = self.current_step >= self.max_steps
         return obs, reward, terminated, truncated, {}
 
